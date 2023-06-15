08df7eb5e3ad041285b2be9a1540b05ad9e5bebf
root <mupao>
2023-06-15 15:42:31 -0700

    n

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,13 +4,13 @@
 /**
  * main : where we start
  * include <stidio.h> : header
- * print : "Programming is like building a multilingual puzzle"
+ * print : "\"Programming is like building a multilingual puzzle"
  * return : 0
  */
 
 int main(void)
 
-{	puts("Programming is like building a multilingual puzzle\n");
+{	puts("\"Programming is like building a multilingual puzzle\n");
 
 	return (0);
 }
