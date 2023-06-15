68037ee16ab690f3664bb338079b576d605ee161
root <mupao>
2023-06-15 15:45:55 -0700

    n

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
-
 /**
  * main : where we start
  * include <stidio.h> : header
  * print : "\"Programming is like building a multilingual puzzle"
- * return : 0
+ * Return : 0
  */
 
 int main(void)
