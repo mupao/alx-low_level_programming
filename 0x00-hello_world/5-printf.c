e661517056adb904dd8f86a8dbcbd401f9c6c7b4
root <mupao>
2023-06-15 16:13:22 -0700

    n

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * description : use printf not put
+ * return : 0
+ */
+
+int main(void)
+{
+	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
