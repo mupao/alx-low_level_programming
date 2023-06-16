7ed1b3e436a956aa3406824ce6f19099e357f5b0
root <mupao>
2023-06-16 10:59:21 -0700

    n

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main: starting point
+ * Description: prints the alphabet in lowercase\n
+ * Return: 0
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+return (0);
+}
