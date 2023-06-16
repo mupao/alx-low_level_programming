b3db5a802949a30055348a677b92ec5bf9688007
root <mupao>
2023-06-16 11:43:17 -0700

    n

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print 0-9 using putchar
+ *              while using int variable
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+{
+
+	int n;
+
+	for (n = 48; n < 58; n++)
+
+	{
+
+		putchar(n);
+
+	}
+
+		putchar('\n');
+
+	return (0);
+
+}
